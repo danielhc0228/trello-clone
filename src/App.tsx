4d48dbca8697a556240554a666840e1e5ef2dba8
Daniel Chung <daniel20020228@gmail.com>
2025-01-16 14:01:08 +1000

    Make Cards move around in a relevant board, just one board

--- src/App.tsx
+++ src/App.tsx
@@ -6,31 +6,35 @@ import Board from "./Components/Board";
 
 const Wrapper = styled.div`
     display: flex;
-    max-width: 680px;
-    width: 100%;
+    width: 100vw;
     margin: 0 auto;
     justify-content: center;
     align-items: center;
     height: 100vh;
 `;
 const Boards = styled.div`
-    display: grid;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
     width: 100%;
-    grid-template-columns: repeat(3, 1fr);
     gap: 10px;
 `;
 
 function App() {
     const [toDos, setToDos] = useRecoilState(toDoState);
-    const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
-        if (!destination) return;
-
-        // setToDos((oldTodos) => {
-        //     const copyTodos = [...oldTodos];
-        //     copyTodos.splice(source.index, 1);
-        //     copyTodos.splice(destination?.index, 0, draggableId);
-        //     return copyTodos;
-        // });
+    const onDragEnd = (info: DropResult) => {
+        const { destination, draggableId, source } = info;
+        if (destination?.droppableId === source.droppableId) {
+            setToDos((allBoard) => {
+                const boardCopy = [...allBoard[source.droppableId]];
+                boardCopy.splice(source.index, 1);
+                boardCopy.splice(destination?.index, 0, draggableId);
+                return {
+                    ...allBoard,
+                    [source.droppableId]: boardCopy,
+                };
+            });
+        }
     };
     return (
         <DragDropContext onDragEnd={onDragEnd}>
