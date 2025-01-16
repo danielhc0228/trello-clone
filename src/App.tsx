816b8a3e39d756526dab8ad90adaa3001d21b042
Daniel Chung <daniel20020228@gmail.com>
2025-01-16 13:10:55 +1000

    Separate Card into a component and create CSS for the board and card

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,66 @@
-import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
+import { DragDropContext, Droppable, DropResult } from "@hello-pangea/dnd";
+import { useRecoilState } from "recoil";
+import styled from "styled-components";
+import { toDoState } from "./atom";
+import DraggableCard from "./Components/DraggableCard";
+
+const Wrapper = styled.div`
+    display: flex;
+    max-width: 480px;
+    width: 100%;
+    margin: 0 auto;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+`;
+const Boards = styled.div`
+    display: grid;
+    width: 100%;
+    grid-template-columns: repeat(1, 1fr);
+`;
+const Board = styled.div`
+    padding: 20px 10px;
+    padding-top: 30px;
+    background-color: ${(props) => props.theme.boardColor};
+    border-radius: 5px;
+    min-height: 200px;
+`;
 
 function App() {
-    const onDragEnd = () => {};
+    const [toDos, setToDos] = useRecoilState(toDoState);
+    const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
+        if (!destination) return;
+
+        setToDos((oldTodos) => {
+            const copyTodos = [...oldTodos];
+            copyTodos.splice(source.index, 1);
+            copyTodos.splice(destination?.index, 0, draggableId);
+            return copyTodos;
+        });
+    };
     return (
         <DragDropContext onDragEnd={onDragEnd}>
-            <div>
-                <Droppable droppableId='one'>
-                    {() => (
-                        <ul>
-                            <Draggable draggableId='first' index={0}>
-                                {() => <li>One</li>}
-                            </Draggable>
-                            <Draggable draggableId='second' index={1}>
-                                {() => <li>Two</li>}
-                            </Draggable>
-                        </ul>
-                    )}
-                </Droppable>
-            </div>
+            <Wrapper>
+                <Boards>
+                    <Droppable droppableId='one'>
+                        {(magic) => (
+                            <Board
+                                ref={magic.innerRef}
+                                {...magic.droppableProps}
+                            >
+                                {toDos.map((toDo, index) => (
+                                    <DraggableCard
+                                        key={toDo}
+                                        toDo={toDo}
+                                        index={index}
+                                    />
+                                ))}
+                                {magic.placeholder}
+                            </Board>
+                        )}
+                    </Droppable>
+                </Boards>
+            </Wrapper>
         </DragDropContext>
     );
 }
---
+++ src/atom.tsx
@@ -0,0 +1,5 @@
+import { atom } from "recoil";
+export const toDoState = atom({
+    key: "toDo",
+    default: ["a", "b", "c", "d", "e", "f"],
+});
